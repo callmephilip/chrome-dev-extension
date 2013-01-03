de9b355db37464f5c98cf86210fc04ba82d267f6
callmephilip <philip.nuzhnyy@gmail.com>
2013-01-03 12:11:50 -0800

    basic dev tools extension - creates a pane in the elements sidebar

---
+++ devtools.js
@@ -0,0 +1,10 @@
+
+chrome.devtools.panels.elements.createSidebarPane( "Dev tools Experiment",
+  function(sidebar) {
+    // function updateElementProperties() {
+    //   sidebar.setExpression("(" + page_getProperties.toString() + ")()");
+    // }  
+    //chrome.devtools.panels.elements.onSelectionChanged.addListener(
+    //updateElementProperties);
+  }
+);
